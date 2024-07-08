79220d5c8d4cbeb61223030c6e388b035aafa9b5
aennivar <aennivar@gmail.com>
2024-07-07 21:32:56 -0300

    script.js adicionado e concluído, imagens não aparecem

---
+++ script.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+mostrarSlides(slideIndex);
+
+// Controles de anterior/próximo
+function somaSlides(n) {
+    mostrarSlides(slideIndex += n);
+}
+
+// Controles de imagem de thumbnail
+function slideAtual(n) {
+    mostrarSlides(slideIndex = n);
+}
+
+function mostrarSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let bolas = document.getElementsByClassName("bola");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex.length = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < bolas.length; i++) {
+        bolas[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
